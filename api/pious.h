763ebf34e0fabd98778d9ebc86892c8e5397ad90
Keldon Alleyne <keldon85@gmail.com>
2016-06-17 00:52:00 +0100

    Put all api code into a single header.

--- api/pious.h
+++ api/pious.h
@@ -1,5 +1,5 @@
 /*
- * Created by The Pious Authors on 12/06/2016.
+ * Created by The Pious Authors on 17/06/2016.
  * MIT License
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,12 +21,10 @@
  * SOFTWARE.
  */
 
-#ifndef PIOUS_PIOUS_TYPES_H
-#define PIOUS_PIOUS_TYPES_H
+#ifndef PIOUS_PIOUS_H
+#define PIOUS_PIOUS_H
 
-struct Pious_Scope;
-struct Pious_UnitSpec;
-struct Pious_UnitIoSpec;
+/** Basic types */
 
 typedef char Pious_S8;
 typedef unsigned char Pious_U8;
@@ -38,6 +36,14 @@ typedef long long Pious_S64;
 typedef unsigned long long Pious_U64;
 typedef float Pious_F32;
 typedef double Pious_F64;
+
+/** Forward declaration */
+
+struct Pious_Scope;
+struct Pious_UnitSpec;
+struct Pious_UnitIoSpec;
+
+/** I/o data types */
 
 /*! \struct   Pious_HoldSignalEvent
  *  \brief    Stores a 'hold signal' event.
@@ -70,7 +76,97 @@ struct Pious_DataPacket {
   char data[4];
 };
 
-/* Pious Scope methods */
+/** Pious Unit */
+
+/* Methods PiousUnits will have to implement **/
+
+/*! \typedef  TPious_UnitPluginSpec
+ *  \brief    Echoes plugin spec to the scope.
+ */
+typedef void (*TPious_UnitPluginSpec)(struct Pious_UnitScope *scope);
+/*! \typedef  TPious_UnitPluginUnit
+ *  \brief    Initializes plugin and returns new data pointer.
+ *
+ *  Initializes the plugin and returns a new pointer to its data to be used by
+ *  the Pious_UnitPluginRender method.
+ */
+typedef void* (*TPious_UnitPluginInit)(struct Pious_UnitScope *scope);
+/*! \typedef  TPious_UnitPluginRender
+ *  \brief    Renders output
+ */
+typedef void (*TPious_UnitPluginRender)(struct Pious_UnitScope *scope, void *data, Pious_S32 frames);
+
+
+enum /* Pious_UnitConstants */ {
+  Pious_UnitNameMax = 256,
+  Pious_UnitShortNameMax = 8,
+  Pious_UnitIoMax = 2048,
+  Pious_UnitIdMax = 128
+};
+
+/*! \enum   Pious_UnitIoType
+ *  \brief  Describes type of io port on a unit.
+ *
+ *  Units can input and output signals (audio / continuous control voltage),
+ *  hold signals (e.g. gate, parameter changes), and data (midi, etc.).
+ *
+ *  Future types may include signal and hold signal arrays.
+ */
+enum Pious_UnitIoType {
+  Pious_UnitIoTypeNone,
+  Pious_UnitIoTypeSignalIn,
+  Pious_UnitIoTypeSignalOut,
+  Pious_UnitIoTypeHoldSignalIn,
+  Pious_UnitIoTypeHoldSignalOut,
+  Pious_UnitIoTypeDataIn,
+  Pious_UnitIoTypeDataOut
+};
+
+
+/*! \struct   Pious_UnitIoSpec
+ *  \brief    Specification for a Unit I/O port.
+ */
+struct Pious_UnitIoSpec {
+  /*! \var    name
+   *  \brief  Full name of io port.
+   */
+  char name[Pious_UnitNameMax];
+  /*! \var    short_name
+   *  \brief  Short name of io port.
+   */
+  char short_name[Pious_UnitShortNameMax];
+  /*! \var    iid
+   *  \brief  Integer id of port.
+   *
+   *  Can be zero or negative, indicating no integer id.
+   *  IO port must have at least one valid method of id.
+   */
+  Pious_S32 iid;
+  /*! \var    sid
+   *  \brief  String id of port.
+   *
+   *  Can be zero length, indicating no string id.
+   *  IO port must have at least one valid method of id.
+   */
+  char sid[Pious_UnitIdMax];
+  /*  \var    io_type
+   *  \brief  Type of io.
+   *
+   *  See Pious_IoType enum for
+   */
+  char io_type;
+};
+
+/*! \struct   Pious_UnitSpec
+ *  \brief    Specification for Unit.
+ */
+struct Pious_UnitSpec {
+  char name[Pious_UnitNameMax];
+  char short_name[Pious_UnitShortNameMax];
+};
+
+
+/** Pious Scope methods */
 
 /*! \typedef  TPious_GetSampleRate
  *  \brief    Returns samplerate of scope.
@@ -109,5 +205,4 @@ struct Pious_Scope {
   TPious_AddUnitIoSpec AddUnitIoSpec;
 };
 
-
-#endif /*PIOUS_PIOUS_TYPES_H*/
+#endif /*PIOUS_PIOUS_H*/
