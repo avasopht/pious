888057cb339412e7aafcc1dbe04762574c26deee
Keldon Alleyne <keldon@itchyjumper.com>
2018-12-21 16:21:38 +0000

    Created first Object test.

--- test/emcee/test_object.cpp
+++ test/emcee/test_object.cpp
@@ -3,3 +3,25 @@
 //
 
 #include <gtest/gtest.h>
+#include <emcee/object.hpp>
+#include <emcee/platform.hpp>
+#include <boost/move/make_unique.hpp>
+#include <boost/move/unique_ptr.hpp>
+
+class ObjectTest : public ::testing::Test {
+ protected:
+
+  virtual void SetUp() {
+    platform_ = boost::movelib::make_unique<emcee::DefaultPlatform>();
+  }
+
+  virtual void TearDown() {
+
+  }
+
+  boost::movelib::unique_ptr<emcee::Platform>  platform_;
+};
+
+TEST_F(ObjectTest, CanInstantiateAnObject) {
+  auto obj = emcee::Object(*platform_);
+}
